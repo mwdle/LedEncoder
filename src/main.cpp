c462f9651d0a4385c09eeb6d5f8788bc73fe36bc
Michael Wadley <michael@mwdle.com>
2024-03-03 16:47:19 -0700

    Fixed bugs & issues with use of WebSerial Library. Fixed potentiometer (brightness control) issues.

--- src/main.cpp
+++ src/main.cpp
@@ -11,20 +11,23 @@
 #include <WebSerial.h>
 
 #include <EncoderStepCounter.h>
+#include <string>
+
 #define ENCODER_PIN1 4
 #define ENCODER_PIN2 5
 #define button 0
-// #define pot A0
-// Create encoder instance:
+#define pot A0
+
+// Initialize rotary encoder
 EncoderStepCounter encoder(ENCODER_PIN1, ENCODER_PIN2);
 
 unsigned long lastButtonPress = 0;
-unsigned long lastCheck = 0;
+unsigned long lastPrint = 0;
 int lastpos = 195;
-// int potPos = analogRead(pot);
+int lastPotPos = analogRead(pot);
 
-const char* ssid = "";
-const char* password = "";
+const char* ssid = "WWN";
+const char* password = "2163East6595SouthSlC84121";
 
 IPAddress subnet(255, 255, 255, 0);
 IPAddress gateway(192, 168, 0, 1);
@@ -58,13 +61,13 @@ void onOTAEnd(bool success) {
 void recvMsg(uint8_t *data, size_t len){
   WebSerial.println("Received Data...");
   String d = "";
-  for(int i=0; i < len; i++){
+  for(size_t i=0; i < len; i++){
     d += char(data[i]);
   }
   WebSerial.println(d);
 }
 
-void HSVtoRGB(float H, float S,float V){
+void HSVtoRGB(float H, float S,float V) {
     if(H>360 || H<0 || S>100 || S<0 || V>100 || V<0){
         return;
     }
@@ -114,22 +117,30 @@ void setup(void) {
     delay(500);
   }
   Serial.println("Wifi Connected! IP Address: " + WiFi.localIP().toString());
+
+  // Initialize ElegantOTA
   ElegantOTA.begin(&server);
   ElegantOTA.onStart(onOTAStart);
   ElegantOTA.onProgress(onOTAProgress);
   ElegantOTA.onEnd(onOTAEnd);
 
+  // Initialize WebSerial
   WebSerial.begin(&server);
   /* Attach Message Callback */
   WebSerial.msgCallback(recvMsg);
+
+  // Start server
   server.begin();
 
-  // LED Setup
+  // LED pin Setup
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
+
+  // Initialize Encoder Button Pin
   pinMode(button, INPUT_PULLUP);
-  // Initialize encoder
+
+  // Initialize rotary encoder
   encoder.begin();
 }
 
@@ -140,32 +151,25 @@ void setColor(int red, int green, int blue) {
   analogWrite(bluePin, 255 - blue);    // Invert the logic for common anode
 }
 
-// void readPot() {
-//    if( millis() % 50 != 0 )
-//        return;
-//     potPos = map(analogRead(pot), 0, 1023, 0, 100);
-//     WebSerial.println("Pot pos: " + potPos);
-// }
-
 void loop(void) {
     ElegantOTA.loop();
 
-    // readPot();
-
     encoder.tick();
-    // read encoder position:
+    // read encoder position and adjust LED color if necessary
     signed char pos = encoder.getPosition() / 3;
-    if (pos != lastpos) {
+    // Read Potentiometer position:
+    int potPos = map(analogRead(pot), 0, 1024, 0, 100);
+    if (lightIsOn && (pos != lastpos || potPos != lastPotPos)) {
       lastpos = pos;
+      lastPotPos = potPos;
       int colorValue = map(lastpos, -42, 42, 60, 330);
-      if( !(millis() % 50 != 0) ) {
-          WebSerial.println("Encoder Pos: " + colorValue);
-          Serial.println("Encoder Pos: " + colorValue);
-      }
-      if (lightIsOn) {
-        HSVtoRGB(colorValue, 100.0, 100.0);
-        setColor(red, green, blue);
-      }
+      HSVtoRGB(colorValue, 100.0, lastPotPos);
+      setColor(red, green, blue);
+    }
+
+    if(millis() - lastPrint > 300) {
+      WebSerial.printf("P-Pos: %d   E-Pos: %d   R: %d   G: %d   B: %d\n", lastPotPos, lastpos, red, green, blue);
+      lastPrint = millis();
     }
 
     int btnState = digitalRead(button);
